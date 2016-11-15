5093eee32fea9dd1de1de54f7286d44d95f9b6f9
MattUhlar <mattuhlar@yahoo.com>
2016-11-14 20:28:48 -0700

    Fixing directory structure. Adding User class. Adding SessionsModel

---
+++ src/common/User.java
@@ -0,0 +1,41 @@
+package common;
+
+/**
+ * Created by mattu on 11/14/16.
+ */
+public class User {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String userType;
+
+    public User(int userId, String firstName, String lastName, String username, String userType) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.userType = userType;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getUserType() {
+        return this.userType;
+    }
+}
---
+++ src/models/Model.java
@@ -0,0 +1,65 @@
+package models;
+
+/**
+ * Created by mattu on 11/13/16.
+ */
+
+import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class Model {
+
+    public Model() throws SQLException, IOException, ClassNotFoundException {
+        dbConnect();
+    }
+
+    protected static Connection conn;
+
+    /**
+     * Gets properties needed to establish connection to DB.
+     * @return properties from config.properties.
+     * @throws IOException
+     */
+    private Properties getProperties() throws IOException {
+
+        Properties properties = new Properties();
+
+        InputStream in = getClass().getResourceAsStream("config.properties");
+        properties.load(in);
+        in.close();
+
+        return properties;
+    }
+
+    /**
+     * Establish connection to mysql database. Requires config.properties with db connection fields.
+     * @return Connection to mysql database
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     * @throws IOException
+     */
+    public void dbConnect() throws SQLException, IOException, ClassNotFoundException {
+
+        Properties dbProperties = getProperties();
+
+        String dbURL    = dbProperties.getProperty("dbURL");
+        String dbUser   = dbProperties.getProperty("dbUser");
+        String dbPass   = dbProperties.getProperty("dbPass");
+        String dbDriver = dbProperties.getProperty("dbDriver");
+
+        Class.forName(dbDriver);
+        this.conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+    }
+
+    /**
+     * Closes connection to DB
+     * @throws SQLException
+     */
+    public void dbDisconnect() throws SQLException {
+        conn.close();
+    }
+}
