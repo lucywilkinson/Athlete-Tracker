bd2bb748f330629a206221297fee5be898c0934b
Dmytro Ryzhkov <dryzhko@gmail.com>
2016-11-18 12:11:05 -0700

    Starting Controllers

---
+++ src/controllers/ProfileController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import models.UserModel;
+import views.ProfileView;
+import common.User;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class ProfileController {
+
+
+}
