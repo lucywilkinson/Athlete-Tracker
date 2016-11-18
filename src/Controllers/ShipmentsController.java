66f42a21b0f2e614e902df122e13999504933bc9
Dmytro Ryzhkov <dryzhko@gmail.com>
2016-11-18 11:37:44 -0700

    Creating Controllers

---
+++ src/Controllers/ShipmentsController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import models.SessionsModel;
+import views.sessionsView;
+import common.User;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ShipmentsController {
+
+	
+}
