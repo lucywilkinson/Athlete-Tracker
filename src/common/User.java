2d1e5b913ac12c134b57989dc09939fbd5bb2f00
Dmytro Ryzhkov <dryzhko@gmail.com>
2016-11-28 15:15:11 -0700

    Merge branch 'master' into DmytroRyzhkov
    # Conflicts:
    #	.DS_Store
    #	src/.DS_Store

--- src/common/User.java
+++ src/common/User.java
@@ -10,32 +10,33 @@ public class User {
     private String lastName;
     private String username;
     private String userType;
+    private String password;
 
-    public User(int userId, String firstName, String lastName, String username, String userType) {
+    public User(int userId, String firstName, String lastName, String username, String userType, String password) {
         this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.userType = userType;
+        this.password = password;
     }
 
     public int getUserId() {
         return this.userId;
     }
-
     public String getFirstName() {
         return this.firstName;
     }
-
     public String getLastName() {
         return this.lastName;
     }
-
     public String getUsername() {
         return this.username;
     }
-
-    public String getUserType() {
+    public String getUserType()  {
         return this.userType;
+    }
+    public String getPassword() {
+        return this.password;
     }
 }
