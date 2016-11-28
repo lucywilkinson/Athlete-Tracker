35a0911bd3d6950e66810859f041acb03987836a
MattUhlar <mattuhlar@yahoo.com>
2016-11-28 15:21:24 -0700

    Removing print statements

--- src/models/UserModel.java
+++ src/models/UserModel.java
@@ -2,7 +2,12 @@ package models;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.util.Map;
+
+import static java.lang.String.*;
+
 /**
  * Created by mattu on 11/18/16.
  */
@@ -13,13 +18,41 @@ public class UserModel extends Model {
     }
 
     /**
-     * Edits all user info fields in the DB.
-     * @param edits map of userAttributeName -> NewValue
-     * @return true if edit successful. False if edit unsuccessful
+     * Accepts a userID and checks if user exxists in DB.
+     * @param userID ID of user in question. used to determine if user exists.
+     * @return true if user exists, false if user does not exist.
+     * @throws SQLException
      */
-    public boolean editUser(Map<String, String> edits) {
+    private boolean userExists(int userID) throws SQLException {
+        String query = "SELECT user_id FROM users WHERE user_id = ?";
 
-        return true;
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, valueOf(userID));
+        ResultSet res = preparedStatement.executeQuery();
+
+        return res.next();
+    }
+
+    /**
+     * Updates all user fields in DB. Map must contain values for all user fields. To keep previous values in DB, set
+     * the user values to the current value in the edits map.
+     * @param edits: Map of user field name to user field value.
+     * @throws SQLException
+     */
+    public void editUser(Map<String, String> edits) throws SQLException {
+
+        String query = "UPDATE users SET first_name=?, last_name=?, username=?, password=?, active=?, user_type=? WHERE user_id = ?";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+
+        preparedStatement.setString(1, edits.get("first_name"));
+        preparedStatement.setString(2, edits.get("last_name" ));
+        preparedStatement.setString(3, edits.get("username"  ));
+        preparedStatement.setString(4, edits.get("password"  ));
+        preparedStatement.setString(5, edits.get("active"    ));
+        preparedStatement.setString(6, edits.get("user_type" ));
+        preparedStatement.setString(7, edits.get("user_id"   ));
+
+        preparedStatement.executeQuery();
     }
 
     /**
@@ -28,7 +61,19 @@ public class UserModel extends Model {
      * @param userID ID of user to deactivate.
      * @return true if deactivation is successful. false if deactivation fails.
      */
-    public boolean deleteProfile(int userID) {
+    public boolean deleteProfile(int userID) throws SQLException {
+
+        // Query will succeed for any userID. Check user exists before updating table.
+        if (!userExists(userID)) {
+           return false;
+        }
+
+        String query = "UPDATE users SET active = false WHERE user_id = ?";
+
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        preparedStatement.setString(1, valueOf(userID));
+        ResultSet res = preparedStatement.executeQuery();
+
         return true;
     }
 }
