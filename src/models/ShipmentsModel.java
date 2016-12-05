888dbbf6233ee7ed60880e929f4779787c608039
lucywilkinson <luwi8498@colorado.edu>
2016-12-05 15:45:29 -0700

    Update shipments model to populate shipments table

--- src/models/ShipmentsModel.java
+++ src/models/ShipmentsModel.java
@@ -1,52 +1,70 @@
-package controllers;
+package models;
 
-import common.User;
-import models.ShipmentsModel;
-import views.shipmentsCard;
+import common.Product;
 
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.HashMap;
-
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.util.Vector;
 
 /**
- * Created by mattu on 11/29/16. Updated by lucywilkinson on 12/5/16.
+ * Updated by lucywilkinson on 12/5/16.
  */
 
-public class ShipmentsController extends BasicController {
-
-    ShipmentsModel shipmentsModel = new ShipmentsModel();
-    shipmentsCard view;
-    User _user;
-    HashMap actionListeners = new HashMap<String, ActionListener>();
-
-
-    public ShipmentsController(User user) throws SQLException, IOException, ClassNotFoundException{
-        super(user);
-
-        DefaultTableModel tableData = shipmentsModel.buildTableModel(shipmentsModel.getRawShipmentData());
-
-        view = new shipmentsCard(actionListeners);
-        view.populate(tableData);
-
-        masterView.addCard("My Profile", view);
+public class ShipmentsModel {
+    public ShipmentsModel() throws SQLException, IOException, ClassNotFoundException {
+        super();
     }
 
-    private class fillTableAction implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            try {
-                ResultSet rs = shipmentsModel.getRawShipmentData();
-                view.populate(shipmentsModel.buildTableModel(rs));
-            }
-            catch (SQLException e1) {
-                e1.printStackTrace();
-            }
+    public ResultSet getRawShipmentData() throws SQLException {
+        String shipmentFields = "shipments.shipment_creator, " +
+                "shipments.shipment_worker, " +
+                "products.product_name, " +
+                "shipments.shipment_quantity, " +
+                "users.first_name, " +
+                "users.last_name, " +
+                "addresses.address, " +
+                "cities.city_name, " +
+                "cities.state_name, " +
+                "countries.country_name";
+
+        String query = "SELECT " + shipmentFields + " FROM shipments " +
+                "inner join addresses on shipments.shipment_address = addresses.address_id " +
+                "inner join cities on addresses.city_id = cities.city_id " +
+                "inner join countries on countries.country_id = addresses.country_id " +
+                "inner join users on shipments.shipment_reciever = users.user_id " +
+                "inner join products on products.product_id = shipments.shipment_product;";
+
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        ResultSet res = preparedStatement.executeQuery();
+
+        return res;
+    }
+
+    public static DefaultTableModel buildTableModel(ResultSet res) throws SQLException {
+        ResultSetMetaData metaData = res.getMetaData();
+
+        //fill column names
+        Vector<String> columnNames = new Vector<String>();
+        int columnCount = metaData.getColumnCount();
+        for(int column = 1; column<=columnCount; column++) {
+            columnNames.add(metaData.getColumnName(column));
         }
-    }
 
+        //fill table data
+        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+        while(res.next()) {
+            Vector<Object> vector = new Vector<Object>();
+            for(int columnIndex = 1; columnIndex<=columnCount; columnIndex++) {
+                vector.add(res.getObject(columnIndex));
+            }
+            data.add(vector);
+        }
+
+        return new DefaultTableModel(data, columnNames);
+
+    }
 }
