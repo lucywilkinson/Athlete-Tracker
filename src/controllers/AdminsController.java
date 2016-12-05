54de54f736c210c756e8a61446b2006c5f9b963c
Alex Goodwin <agoodwin727@gmail.com>
2016-12-05 15:09:33 -0700

    Added new user functionality to adminsCard

--- src/controllers/AdminsController.java
+++ src/controllers/AdminsController.java
@@ -2,16 +2,30 @@ package controllers;
 
 import common.User;
 import views.adminsCard;
+import views.card;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by alex on 11/27/16.
  */
 public class AdminsController extends BasicController {
+    adminsCard view;
+
     public AdminsController(User user) {
         super(user);
 
-        adminsCard adminsCard = new adminsCard(actionListeners);
-        masterView.addCard("Shipments", adminsCard);
+        actionListeners.put("newUserAction", new newUserAction());
+
+        view = new adminsCard(actionListeners);
+        masterView.addCard("Shipments", view);
     }
 
+    private class newUserAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.launchNewUser();
+        }
+    }
 }
