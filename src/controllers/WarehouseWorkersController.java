a0325233987e74f0594520ac1efe280b1dfaca3f
lucywilkinson <luwi8498@colorado.edu>
2016-12-05 15:14:35 -0700

    Add populate functionality for workers controller

--- src/controllers/WarehouseWorkersController.java
+++ src/controllers/WarehouseWorkersController.java
@@ -1,20 +1,48 @@
 package controllers;
 
 import common.User;
-import models.ProductModel;
+import models.UserModel;
 import views.warehouseWorkersCard;
 
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 /**
- * Created by mattu on 11/29/16.
+ * Created by lucywilkinson on 12/1/16.
  */
+
 public class WarehouseWorkersController extends BasicController {
-    ProductModel productModel;
+    UserModel userModel = new UserModel();
+    warehouseWorkersCard view;
+    User _user;
+    HashMap actionListeners = new HashMap<String, ActionListener>();
 
-    public WarehouseWorkersController(User user) {
-        super();
+    public WarehouseWorkersController(User user) throws SQLException, IOException, ClassNotFoundException {
+        super(user);
 
-        warehouseWorkersCard warehouseWorkersCard = new warehouseWorkersCard(actionListeners);
-        masterView.addCard("WarehouseWorkers", warehouseWorkersCard);
+        DefaultTableModel tableData = userModel.buildTableModel(userModel.returnUsersofType("worker"));
+
+        view = new warehouseWorkersCard(actionListeners);
+        view.populate(tableData);
+
+        masterView.addCard("My Profile", view);
+    }
+
+    private class fillTableAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                ResultSet rs = userModel.returnUsersofType("worker");
+                view.populate(userModel.buildTableModel(rs));
+            }
+            catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
 
 }
