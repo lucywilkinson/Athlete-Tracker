b0cd708b45eb7d4fcef3df3d5332aa6f69363ef9
Alex Goodwin <agoodwin727@gmail.com>
2016-12-05 21:45:51 -0700

    Added new/edit functionality to workersCard and WorkersController

--- src/controllers/WarehouseWorkersController.java
+++ src/controllers/WarehouseWorkersController.java
@@ -5,9 +5,11 @@ import models.ProductModel;
 import models.UserModel;
 import views.warehouseWorkersCard;
 
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,39 +18,99 @@ import java.util.HashMap;
 public class WarehouseWorkersController extends BasicController {
     UserModel userModel = new UserModel();
     warehouseWorkersCard view;
-    User _user;
-
 
     public WarehouseWorkersController() throws SQLException, IOException, ClassNotFoundException {
 
-        DefaultTableModel tableData = userModel.buildTableModel("worker");
-
         actionListeners.put("newUserAction", new newUserAction());
+        actionListeners.put("saveNewUserAction", new saveNewUserAction());
+        actionListeners.put("editWorkerAction", new editWorkerAction());
+        actionListeners.put("saveEditWorkerAction", new saveEditsAction());
 
         view = new warehouseWorkersCard(actionListeners);
-        masterView.addCard("WarehouseWorkers", view);
+        masterView.addCard("Workers", view);
 
-        view = new warehouseWorkersCard(actionListeners);
-        view.populate(tableData);
+        view.dataTable.setModel(userModel.buildTableModel("worker"));
     }
-
-
-    private class fillTableAction implements ActionListener {
+    private class newUserAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            try {
-                view.populate(userModel.buildTableModel("worker"));
+            view.launchNewUser();
+        }
+    }
+
+    private class saveNewUserAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String firstName = view.newUserFirstNameField.getText();
+            String lastName  = view.newUserLastNameField.getText();
+            String username  = view.newUserUsernameField.getText();
+            String email     = view.newUserEmailField.getText();
+            String userType  = String.valueOf(view.newUserAccountTypeField.getSelectedItem());
+            String password  = String.valueOf(view.newUserPasswordField.getPassword());
+            String confirmPassword  = String.valueOf(view.newUserConfirmPasswordField.getPassword());
+
+            if (!password.equals(confirmPassword)) {
+                JOptionPane.showMessageDialog(new JFrame(), "Passwords do not match", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
             }
-            catch (SQLException e1) {
+
+            try {
+                User newUser = new User(firstName, lastName, username, userType, password, email);
+                userModel.addUser(newUser);
+                view.newUserFrame.dispatchEvent(new WindowEvent(view.newUserFrame, WindowEvent.WINDOW_CLOSING));
+                view.dataTable.setModel(userModel.buildTableModel(userType));
+
+            } catch (SQLException e1) {
                 e1.printStackTrace();
             }
         }
     }
 
-    private class newUserAction implements ActionListener {
+    private class editWorkerAction implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            view.launchNewUser();
+            view.launchEditUser();
+            int row = view.dataTable.getSelectedRow();
+
+            String id        = String.valueOf(view.dataTable.getValueAt(row, 0));
+            String firstName = String.valueOf(view.dataTable.getValueAt(row, 1));
+            String lastName  = String.valueOf(view.dataTable.getValueAt(row, 2));
+            String username  = String.valueOf(view.dataTable.getValueAt(row, 3));
+            String password  = String.valueOf(view.dataTable.getValueAt(row, 4));
+            String email     = String.valueOf(view.dataTable.getValueAt(row, 5));
+
+            view.editUserIdField.setText(id);
+            view.editUserFirstNameField.setText(firstName);
+            view.editUserLastNameField.setText(lastName);
+            view.editUserUsernameField.setText(username);
+            view.editUserPasswordField.setText(password);
+            view.editUserEmailField.setText(email);
+        }
+    }
+
+    private class saveEditsAction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int    id        = Integer.parseInt(view.editUserIdField.getText());
+            String firstName = view.editUserFirstNameField.getText();
+            String lastName  = view.editUserLastNameField.getText();
+            String username  = view.editUserUsernameField.getText();
+            String email     = view.editUserEmailField.getText();
+            String userType  = String.valueOf(view.editUserAccountTypeField.getSelectedItem());
+            String password  = String.valueOf(view.editUserPasswordField.getPassword());
+
+            User updatedUser = new User(id, firstName, lastName, username, password, email, userType);
+
+            try {
+                // Update user in DB
+                userModel.editUser(updatedUser);
+                view.editUserFrame.dispatchEvent(new WindowEvent(view.editUserFrame, WindowEvent.WINDOW_CLOSING));
+                view.dataTable.setModel(userModel.buildTableModel(userType));
+
+
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 }
